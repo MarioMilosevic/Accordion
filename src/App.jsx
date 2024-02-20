2f5e708bc46a80dbb3e26097ecafdc97f0d1a83d
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-20 12:35:58 +0100

    Add data and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import './App.css'
+import { useState } from "react";
+import Question from "./components/Question";
+import questions from "./data/data";
 
 function App() {
+  console.log(questions)
+  const [appQuestions,setAppQuestions] = useState()
+  
+
+
   return (
     <>
-    <main className='border bg-white '> 
-    <h1>Questions</h1>
-    </main>
-    
+      <main className="border bg-white p-4 rounded-lg shadow-lg">
+        <h1 className="text-center p-4 text-4xl">Questions</h1>
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Question.jsx
@@ -0,0 +1,12 @@
+const Question = () => {
+  return (
+    <div className="w-[700px] flex items-center justify-between px-4 py-2 my-6 mx-4 shadow-lg outline outline-blue-200 rounded-lg">
+      <p>Do I Have To Allow The Use Of Cookies?</p>
+      <button className="rounded-full bg-blue-400 px-2 py-1 text-xl text-white">
+        +
+      </button>
+    </div>
+  );
+};
+
+export default Question;
